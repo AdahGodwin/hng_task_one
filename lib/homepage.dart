f1076a34aa156a3071d2c3a4bb8422c18d6a4e2a
Godwin Adah <adahgodwin224@gmail.com>
2023-09-08 08:10:38 +0100

    permissions added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,49 +13,51 @@ class HomePage extends StatelessWidget {
       body: LayoutBuilder(
         builder: (context, constraints) => Padding(
           padding: const EdgeInsets.all(16.0),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.9,
-            width: MediaQuery.of(context).size.width > 600
-                ? 500
-                : constraints.maxWidth,
-            child: Card(
-              elevation: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(25),
-                      child: Image.asset(
-                        "assets/images/profile.jpg",
-                        fit: BoxFit.cover,
-                        width: 250,
-                        height: 250,
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(16),
-                      child: const Text(
-                        "Godwin Adah",
-                        style: TextStyle(
-                            fontSize: 26, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(WebViewPage.routeName);
-                      },
-                      child: const Text(
-                        "Open Github",
-                        style: TextStyle(
-                          fontSize: 16,
+          child: Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.9,
+              width: MediaQuery.of(context).size.width > 600
+                  ? 500
+                  : constraints.maxWidth,
+              child: Card(
+                elevation: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: Image.asset(
+                          "assets/images/profile.jpg",
+                          fit: BoxFit.cover,
+                          width: 250,
+                          height: 250,
                         ),
                       ),
-                    ),
-                    
-                  ],
+                      Container(
+                        margin: const EdgeInsets.all(16),
+                        child: const Text(
+                          "Godwin Adah",
+                          style: TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(WebViewPage.routeName);
+                        },
+                        child: const Text(
+                          "Open Github",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      
+                    ],
+                  ),
                 ),
               ),
             ),
